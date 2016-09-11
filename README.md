c2ad96c4bbf77e3c33118c21272967970aadc8b0
pboettcher <relay20042004@mail.ru>
2016-09-11 16:48:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# WriteDateRename
+
+This application is designed to rename all the files in current directory according to their write dates exact to the milliseconds.
+
+The app is intended to reside in one of PATH directories to be called from cmd console.
+
+Running scenario is as follows:
+
+1. run cmd and change to the desired directory.
+2. Execute WriteDateRename. The app runs and warns about the current working path.
+3. As a protection measure you have to type the word "continue" in because the app does mass-rename and results of such a renaming aren't reversible.
+4. Once you typed the protection word in the job is done quickly.
---
+++ WriteDateRename/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteDateRename
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string curDir = Directory.GetCurrentDirectory();
+            Console.WriteLine("This app will rename files in current directory according to their write timestamp.\nCurrent directory is {0}\nType \"continue\" if you want to proceed.", curDir);
+            string userInput = Console.ReadLine();
+            if (userInput=="continue")
+            {
+                string[] files = Directory.GetFiles(curDir);
+                foreach (string fileName in files)
+                {
+                    DateTime wt = File.GetLastWriteTime(fileName);
+                    try
+                    {
+                        File.Move(fileName, string.Format("{0:yyyyMMdd_HHmmss_fff}{1}", wt, Path.GetExtension(fileName)));
+                    }
+                    catch (IOException ex)
+                    {
+                        Console.WriteLine("from:\"{0}\" to:\"{1:yyyyMMdd_HHmmss}{2}\" {3}\n", Path.GetFileName(fileName), wt, Path.GetExtension(fileName), ex.Message);
+                        try
+                        {
+                            File.Move(fileName, string.Format("{0:yyyyMMdd_HHmmss} {1}", wt, Path.GetFileName(fileName)));
+                        }
+                        catch (IOException ex1)
+                        {
+                            Console.WriteLine("from:\"{0}\" to:\"{1:yyyyMMdd_HHmmss}{2}\" {3}\n", Path.GetFileName(fileName), wt, Path.GetExtension(fileName), ex1.Message);
+                        }
+                    }
+                }
+            }
+            else
+                Console.WriteLine("No action performed. Exiting.");
+        }
+    }
+}
